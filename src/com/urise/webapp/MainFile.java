281e18624764c4476dca5428f428bf6fb055365b
Taras_V Makarov <tvmakarov@mail.ru>
2020-11-06 22:33:49 +0300

    corrected recursion (Lesson_9)

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -3,6 +3,9 @@ package com.urise.webapp;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainFile {
 
@@ -16,27 +19,44 @@ public class MainFile {
             throw new RuntimeException("Error", e);
         }
 
+
+        String dirPath = "./src/com/urise/webapp";
+        File fileOrDir = new File(dirPath);
+
         try (FileInputStream fis = new FileInputStream(filePath)) {
             System.out.println(fis.read());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        String dirPath = "./src/com/urise/webapp";
-        File fileOrDir = new File(dirPath);
-
         workWithFile(fileOrDir, "");
     }
 
-    static void workWithFile(File fileOrDir, String gap) {
-        File[] files = fileOrDir.listFiles();
 
+    static void workWithFile(File fileOrDir, String gap) {
+
+        File[] files = fileOrDir.listFiles();
         if (files != null) {
+            Arrays.sort(files);
+            List<File> onlyFiles = new ArrayList<>();
+            List<File> onlyDir = new ArrayList<>();
+            List<File> filesAndDir = new ArrayList<>();
+
             for (File file : files) {
+                if (file.isDirectory()) {
+                    onlyDir.add(file);
+                } else if (file.isFile()) {
+                    onlyFiles.add(file);
+                }
+            }
+            filesAndDir.addAll(onlyDir);
+            filesAndDir.addAll(onlyFiles);
+
+            for (File file : filesAndDir) {
                 if (file.isFile()) {
                     System.out.println(gap + file.getName());
-                } else {
-                    System.out.println(gap + file.getName());
+                } else if (file.isDirectory()) {
+                    System.out.println(file.getName());
                     workWithFile(file, " ");
                 }
             }
