a5c8d47e7b7f2de1f810c07f99485e1cefd99bde
Taras_V Makarov <tvmakarov@mail.ru>
2020-12-04 18:19:58 +0300

    LazySingleton - Lesson_11

---
+++ src/com/urise/webapp/LazySingleton.java
@@ -0,0 +1,28 @@
+package com.urise.webapp;
+
+public class LazySingleton {
+
+    volatile private static LazySingleton INSTANCE;
+
+    private LazySingleton() {
+    }
+
+    private static class LazySingletonHolder {
+        private static final LazySingleton INSTANCE = new LazySingleton();
+    }
+
+    public static LazySingleton getINSTANCE() {
+        return LazySingletonHolder.INSTANCE;
+    }
+
+    //    public static LazySingleton getInstance() {
+//        if (INSTANCE == null) {
+//            synchronized (LazySingleton.class) {
+//                if (INSTANCE == null) {
+//                    INSTANCE = new LazySingleton();
+//                }
+//            }
+//        }
+//        return INSTANCE;
+//    }
+}
