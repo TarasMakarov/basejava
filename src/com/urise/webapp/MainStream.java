425face3d30eeb995f125c81039c6766557580ef
Taras_V Makarov <tvmakarov@mail.ru>
2020-12-11 22:19:33 +0300

    update MainStream - HW_12

--- src/com/urise/webapp/MainStream.java
+++ src/com/urise/webapp/MainStream.java
@@ -1,40 +1,28 @@
 package com.urise.webapp;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class MainStream {
     public static void main(String[] args) {
         int[] intArr = new int[]{1, 2, 5, 5, 1, 8};
         System.out.println(minValue(intArr));
-        List<Integer> integerList = Arrays.asList(1, 6, 5, 4, 3, 9);
+        List<Integer> integerList = Arrays.asList(1, 3, 5, 4, 4, 8);
         System.out.println(oddOrEven(integerList));
     }
 
     private static int minValue(int[] values) {
-        return Arrays.stream(values).sorted().distinct().reduce(0, (x, y) -> x * 10 + y);
+        return Arrays.stream(values)
+                .sorted()
+                .distinct()
+                .reduce(0, (x, y) -> x * 10 + y);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        int sum = IntStream.range(0, integers.size()).sum();
-        ArrayList<Integer> list;
-        if (sum % 2 != 0) {
-            list = (ArrayList<Integer>) integers.stream()
-                    .filter(i -> i % 2 != 0)
-                    .collect(Collectors.toList());
-        } else {
-            list = (ArrayList<Integer>) integers.stream()
-                    .filter(i -> i % 2 == 0)
-                    .collect(Collectors.toList());
-        }
-        return list;
+        return integers.stream()
+                .filter(integers.stream().mapToInt(Integer::intValue)
+                        .sum() % 2 == 0 ? x -> x % 2 != 0 : x -> x % 2 == 0)
+                .collect(Collectors.toList());
     }
 }
-//        1. to stream
-//        2. sum
-//        3. filter
-//        4. delete
-//        5. to List
