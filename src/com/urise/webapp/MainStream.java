6a2da2491cef8ca54bc079036fb0bb75a5e401ad
Taras_V Makarov <tvmakarov@mail.ru>
2020-12-13 21:59:59 +0300

    update oddOrEven() in MainStream - HW_12

--- src/com/urise/webapp/MainStream.java
+++ src/com/urise/webapp/MainStream.java
@@ -8,7 +8,7 @@ public class MainStream {
     public static void main(String[] args) {
         int[] intArr = new int[]{1, 2, 5, 5, 1, 8};
         System.out.println(minValue(intArr));
-        List<Integer> integerList = Arrays.asList(8, 4, 5, 4, 2, 9);
+        List<Integer> integerList = Arrays.asList(7, 4, 5, 4, 2, 9);
         System.out.println(oddOrEven(integerList));
     }
 
@@ -24,7 +24,7 @@ public class MainStream {
                 .mapToInt(Integer::intValue)
                 .sum();
         return integers.stream()
-                .filter(sum % 2 == 0 ? x -> x % 2 != 0 : x -> x % 2 == 0)
+                .filter((sum % 2 == 0) ? (x -> x % 2 != 0) : (x -> x % 2 == 0))
                 .collect(Collectors.toList());
 //        return integers.stream()
 //                .filter(integers.stream().mapToInt(Integer::intValue)
