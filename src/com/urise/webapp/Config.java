eb8731e1ad8fd4ad7e3e75c2195447088a4d9140
Taras_V Makarov <tvmakarov@mail.ru>
2020-12-26 20:06:25 +0300

    update all methods of SqlStorage - HW_13

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -1,5 +1,7 @@
 package com.urise.webapp;
 
+import com.urise.webapp.storage.SqlStorage;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,14 +14,17 @@ public class Config {
 
     private Properties props = new Properties();
     private File storageDir;
-//    private File storageDir;
-//    private File storageDir;
-//    private File storageDir;
+    private SqlStorage storage;
 
+
+//    private File storageDir;
+//    private File storageDir;
+//    private File storageDir;
     private Config() {
         try (InputStream is = new FileInputStream(PROPS)) {
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
+            storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
         } catch (IOException e) {
             throw new IllegalStateException("Invalid config file" + PROPS.getAbsolutePath());
         }
@@ -31,5 +36,9 @@ public class Config {
 
     public File getStorageDir() {
         return storageDir;
+    }
+
+    public SqlStorage getStorage() {
+        return storage;
     }
 }
