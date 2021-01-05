07da989ba5efd8e85b4805778ea3dfa3633faaf7
Taras_V Makarov <tvmakarov@mail.ru>
2021-01-05 12:58:37 +0300

    add file populate.sql - HW_13

---
+++ config/populate.sql
@@ -0,0 +1,4 @@
+INSERT INTO resume (uuid, full_name) VALUES
+('5a803e3e-7dfe-454c-84fd-9bd25d602415', 'Name1'),
+('6ced04a5-1b89-49d8-8bcb-8563d9d266af', 'Name2'),
+('bbfcea9d-43db-40a9-a243-c2c8caa92692', 'Name3')
