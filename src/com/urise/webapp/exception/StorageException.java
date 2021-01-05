4c9bc8ee6586e05bb411199742b1d28e4168f86c
Taras_V Makarov <tvmakarov@mail.ru>
2021-01-05 12:29:52 +0300

    remove trash, check - HW_13

--- src/com/urise/webapp/exception/StorageException.java
+++ src/com/urise/webapp/exception/StorageException.java
@@ -3,13 +3,17 @@ package com.urise.webapp.exception;
 public class StorageException extends RuntimeException {
     private final String uuid;
 
+    public StorageException(String message) {
+        this(message, null, null);
+    }
+
     public StorageException(String message, String uuid) {
         super(message);
         this.uuid = uuid;
     }
 
-    public StorageException(String message) {
-        this(message, null, null);
+    public StorageException(Exception e) {
+        this(e.getMessage(), e);
     }
 
     public StorageException(String message, Exception e) {
@@ -19,10 +23,6 @@ public class StorageException extends RuntimeException {
     public StorageException(String message, String uuid, Exception e) {
         super(message, e);
         this.uuid = uuid;
-    }
-
-    public StorageException(Exception e) {
-        this(e.getMessage(), e);
     }
 
     public String getUuid() {
