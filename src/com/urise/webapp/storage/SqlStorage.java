df12b41833dbb1953999dc56687c8537a3c25910
Taras_V Makarov <tvmakarov@mail.ru>
2021-01-08 22:42:34 +0300

    change getAllSorted() - HW_14

--- src/com/urise/webapp/storage/SqlStorage.java
+++ src/com/urise/webapp/storage/SqlStorage.java
@@ -1,13 +1,14 @@
 package com.urise.webapp.storage;
 
 import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.model.ContactType;
 import com.urise.webapp.model.Resume;
 import com.urise.webapp.sql.SqlHelper;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class SqlStorage implements Storage {
 
@@ -38,25 +39,48 @@ public class SqlStorage implements Storage {
 
     @Override
     public void save(Resume r) throws Exception {
-        sqlHelper.execute("INSERT INTO  resume (uuid, full_name) VALUES (?, ?)", preparedStatement -> {
-            preparedStatement.setString(1, r.getUuid());
-            preparedStatement.setString(2, r.getFullName());
-            preparedStatement.execute();
-            return null;
-        });
+        sqlHelper.transactionalExecute(conn -> {
+                    try (PreparedStatement ps = conn.prepareStatement("INSERT INTO  resume (uuid, full_name) VALUES (?, ?)")) {
+                        ps.setString(1, r.getUuid());
+                        ps.setString(2, r.getFullName());
+                        ps.execute();
+                    }
+                    try (PreparedStatement ps = conn.prepareStatement("INSERT INTO  contact (resume_uuid, type, value) VALUES (?, ?, ?)")) {
+                        for (Map.Entry<ContactType, String> e : r.getContactsMap().entrySet()) {
+                            ps.setString(1, r.getUuid());
+                            ps.setString(2, e.getKey().name());
+                            ps.setString(3, e.getValue());
+                            ps.addBatch();
+                        }
+                        ps.executeBatch();
+                    }
+                    return null;
+                }
+        );
     }
 
     @Override
     public Resume get(String uuid) {
-        return sqlHelper.execute("SELECT * FROM resume r WHERE r.uuid = ?", preparedStatement -> {
-            preparedStatement.setString(1, uuid);
-            ResultSet rs = preparedStatement.executeQuery();
-            if (!rs.next()) {
-                throw new NotExistStorageException(uuid);
-            }
-            Resume r = new Resume(uuid, rs.getString("full_name"));
-            return r;
-        });
+        return sqlHelper.execute("" +
+                        "SELECT * FROM resume r " +
+                        "LEFT JOIN contact c " +
+                        "    ON r.uuid = c.resume_uuid" +
+                        " WHERE r.uuid = ?",
+                preparedStatement -> {
+                    preparedStatement.setString(1, uuid);
+                    ResultSet rs = preparedStatement.executeQuery();
+                    if (!rs.next()) {
+                        throw new NotExistStorageException(uuid);
+                    }
+                    Resume r = new Resume(uuid, rs.getString("full_name"));
+                    do {
+                        ContactType type = ContactType.valueOf(rs.getString("type"));
+                        String value = rs.getString("value");
+                        r.setContacts(type, value);
+                    } while (rs.next());
+                    return r;
+                });
+
     }
 
     @Override
@@ -72,12 +96,23 @@ public class SqlStorage implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
-        return sqlHelper.execute("SELECT * FROM resume r ORDER BY full_name, uuid", preparedStatement -> {
+        return sqlHelper.execute("" +
+                "SELECT * FROM resume r " +
+                "LEFT JOIN contact c " +
+                "ON r.uuid = c.resume_uuid " +
+                "ORDER BY full_name, uuid", preparedStatement -> {
             ResultSet rs = preparedStatement.executeQuery();
-            List<Resume> list = new ArrayList<>();
+            Map<String, Resume> resumeMap = new HashMap<>();
             while (rs.next()) {
-                list.add(new Resume(rs.getString("uuid"), rs.getString("full_name")));
+                String uuid = rs.getString("uuid");
+                Resume r = new Resume(uuid, rs.getString("full_name"));
+                resumeMap.put(uuid, r);
+                String value = rs.getString("value");
+                ContactType type = ContactType.valueOf(rs.getString("type"));
+                r.setContacts(type, value);
             }
+            ArrayList<Resume> list = new ArrayList<>(resumeMap.values());
+            Collections.sort(list);
             return list;
         });
     }
