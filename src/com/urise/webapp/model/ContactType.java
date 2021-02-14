79ceb52b8f1ab4acd42643dd0389a6d69b5eb429
Taras_V Makarov <tvmakarov@mail.ru>
2021-02-14 18:27:27 +0300

    crud - Lesson_16

--- src/com/urise/webapp/model/ContactType.java
+++ src/com/urise/webapp/model/ContactType.java
@@ -2,9 +2,20 @@ package com.urise.webapp.model;
 
 public enum ContactType {
 
-    PHONE("Тел.:"),
-    SKYPE("Skype:"),
-    EMAIL("Почта:"),
+    PHONE("Тел."),
+    MOBILE("Мобильный"),
+    SKYPE("Skype:") {
+        @Override
+        public String toHtmlNotNull(String value) {
+            return "<a href= skype:" + value + ">" + value + "</a>";
+        }
+    },
+    EMAIL("Почта:") {
+        @Override
+        public String toHtmlNotNull(String value) {
+            return "<a href= mailto:" + value + ">" + value + "</a>";
+        }
+    },
     LINKEDIN("Профиль LinkedIn: "),
     GITHUB("Профиль GitHub: "),
     STACKOVERFLOW("Профиль Stackoverflow: "),
@@ -18,5 +29,13 @@ public enum ContactType {
 
     public String getTitle() {
         return title;
+    }
+
+    protected String toHtmlNotNull(String value) {
+        return title + ": " + value;
+    }
+
+    public String toHtml(String value) {
+        return (value == null) ? "" : toHtmlNotNull(value);
     }
 }
