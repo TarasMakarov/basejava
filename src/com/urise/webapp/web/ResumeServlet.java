214de1cfc4613166cd3b37393eabbe0c5126b52e
Taras_V Makarov <tvmakarov@mail.ru>
2021-04-15 06:18:52 +0300

    delete empty strings in section - HW_16

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -59,7 +59,7 @@ public class ResumeServlet extends HttpServlet {
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        r.setSection(type, new BulletListSection(value.split("\r\n\r")));
+                        r.setSection(type, new BulletListSection(value.split("[\r\n]+")));
                         break;
                     case EDUCATION:
                     case EXPERIENCE:
